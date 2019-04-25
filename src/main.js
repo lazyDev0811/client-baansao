b40a059ae8617cc44601ed6130b6eeab44e734e9
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-04-25 05:40:12 -0700

    Strip out any jQuery garbage.

--- src/main.js
+++ src/main.js
@@ -4,34 +4,11 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
 import '~/assets/scss/style.scss'
-
 import '~/assets/css/style.css'
-import '~/assets/css/jquery-ui.css'
-import '~/assets/css/magnific-popup.css'
 import '~/assets/fonts/icomoon/style.css'
-
-//import AOS from "aos";
-//import "~/assets/css/aos.css";
-//AOS.init();
-
-//import carousel from 'vue-owl-carousel'
 
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-  //Vue.component(carousel)
-
-  /*head.script.push({ src: '/assets/js/bootstrap-datepicker.js' })
-  head.script.push({ src: '/assets/js/bootstrap.min.js' })
-  head.script.push({ src: '/assets/js/jquery-3.3.1.min.js' })
-  head.script.push({ src: '/assets/js/jquery-migrate-3.0.1.min.js' })
-  head.script.push({ src: '/assets/js/jquery-ui.js' })
-  head.script.push({ src: '/assets/js/jquery.countdown.min.js' })
-  head.script.push({ src: '/assets/js/jquery.magnific-popup.min.js' })
-  head.script.push({ src: '/assets/js/jquery.stellar.min.js' })
-  head.script.push({ src: '/assets/js/main.js' })
-  head.script.push({ src: '/assets/js/popper.min.js' })
-  head.script.push({ src: '/assets/js/slick.min.js' })*/
-
 }
