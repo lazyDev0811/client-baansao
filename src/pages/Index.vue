0f3ebcf7fd991aa90b9292df4849ab03eca0ed63
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-04-25 04:55:31 -0700

    Layout hacks some thing is weird with positioning.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,66 +9,66 @@
   </head>
 
   <body>
-    <video autoplay muted loop id="bg-video">
-      <source src="/video/032715490-dandelion-blowing-slow-motion_H264_420-web-xl.mp4" type="video/mp4">
-    </video>
-    <!--<div class="site-blocks-cover overlay aos-init aos-animate" style="background-image: url(images/hero_white.jpg)" data-aos="fade" data-stellar-background-ratio="0.5">-->
-    <div class="site-blocks-cover overlay aos-init aos-animate" data-aos="fade" data-stellar-background-ratio="0.5">
-      <div class="container">
-        <div class="row align-items-center text-center justify-content-center" style="position: relative; top: -3rem;">
-          <div class="col-md-8">
-            <h1>Wilder than the Wind</h1>
-            <span class="sub-text">creation.inspiration.education.elevation.</span>
+    <div class="hero-wrapper">
+      <video autoplay muted loop id="bg-video">
+        <source src="/video/032715490-dandelion-blowing-slow-motion_H264_420-web-xl.mp4" type="video/mp4">
+      </video>
+      <!--<div class="site-blocks-cover overlay aos-init aos-animate" style="background-image: url(images/hero_white.jpg)" data-aos="fade" data-stellar-background-ratio="0.5">-->
+      <div class="site-blocks-cover overlay aos-init aos-animate" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center text-center justify-content-center" style="position: relative; top: -3rem;">
+            <div class="col-md-8">
+              <h1>Wilder than the Wind</h1>
+              <span class="sub-text">creation.inspiration.education.elevation.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-block-1">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <a href="/classes" class="site-block-feature d-flex p-4 rounded mb-4">
+                <div class="mr-3">
+                  <i class="material-icons" style="font-size:40px;color:white;">brush</i>
+                </div>
+                <div class="text">
+                  <h3>Art Workshops</h3>
+                  <p>Check out our current classes on offer</p>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-4">
+              <a href="#" class="site-block-feature d-flex p-4 rounded mb-4">
+                <div class="mr-3">
+                  <i class="material-icons" style="font-size:40px;color:white;">calendar_today</i>
+                </div>
+                <div class="text">
+                  <h3>Events Calendar</h3>
+                  <p>Explore our events page to find a class that will fit your schedule.</p>
+                </div>
+              </a>
+            </div>
+            <div class="col-lg-4">
+              <a href="#" class="site-block-feature d-flex p-4 rounded mb-4">
+                <div class="mr-3">
+                  <i class="material-icons" style="font-size:40px;color:white;">local_grocery_store</i>
+                </div>
+                <div class="text">
+                  <h3>Art for Sale</h3>
+                  <p>Coming soon!</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    <div class="site-block-1">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <a href="/classes" class="site-block-feature d-flex p-4 rounded mb-4">
-              <div class="mr-3">
-                <i class="material-icons" style="font-size:40px;color:white;">brush</i>
-              </div>
-              <div class="text">
-                <h3>Art Workshops</h3>
-                <p>Check out our current classes on offer</p>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4">
-            <a href="#" class="site-block-feature d-flex p-4 rounded mb-4">
-              <div class="mr-3">
-                <i class="material-icons" style="font-size:40px;color:white;">calendar_today</i>
-              </div>
-              <div class="text">
-                <h3>Events Calendar</h3>
-                <p>Explore our events page to find a class that will fit your schedule.</p>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4">
-            <a href="#" class="site-block-feature d-flex p-4 rounded mb-4">
-              <div class="mr-3">
-                <i class="material-icons" style="font-size:40px;color:white;">local_grocery_store</i>
-              </div>
-              <div class="text">
-                <h3>Art for Sale</h3>
-                <p>Coming soon!</p>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
 
     <div class="site-section">
-      <div class="container">
-        <div class="row">
+      <div class="container pt-4">
+        <div class="row mt-4 mb-4 pt-4">
           <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="img-border">
               <img src="/images/oceanbeach.jpg" alt="Ocean Beach Art" class="img-fluid">
@@ -135,46 +135,44 @@
     </div>
 
 
-<div class="container site-section block-13 testimonial-wrap">
-   <div class="row">
-      <div class="col-12 text-center">
-         <span class="sub-title">Happy Students</span>
-         <h2 class="font-weight-bold text-black mb-5">Testimonials</h2>
-      </div>
-   </div>
-   <div class="nonloop-block-13 owl-carousel owl-loaded owl-drag">
-      <div class="owl-stage-outer">
-         <!--<carousel>
-            <div class="owl-item active" style="width: 380px;">
-               <div class="testimony">
-                  <img src="/images/reannin_sirianni.jpg" alt="Image" class="img-fluid">
-                  <h3>Reannin Sirianni</h3>
-                  <span class="sub-title"></span>
-                  <p>“<em>I recently took an art class here. It was ridiculously amazing. I had so much fun! I felt like a little kid creating my artwork. Stephanie is such an awesome teacher she teaches you many practical techniques but also gives you plenty of time to play around make a mess and encourages you to have fun. Stephanie’s energy is fabulous. I highly recommend taking a class here to everyone and anyone. I loved it!! I will definitely be back!</em>”</p>
-               </div>
-            </div>
-            <div class="owl-item active" style="width: 380px;">
-               <div class="testimony">
-                  <img src="/images/nellie_thompson.jpg" alt="Image" class="img-fluid">
-                  <h3>Nellie Thompson</h3>
-                  <span class="sub-title"></span>
-                  <p>“<em>I met Stephanie at a market and loved her creations. Took the flowers and landscape class. Loved it and can’t wait to take more.</em>”</p>
-               </div>
-            </div>
-            <div class="owl-item active" style="width: 380px;">
-               <div class="testimony">
-                  <img src="/images/julie_d_king.jpg" alt="Image" class="img-fluid">
-                  <h3>Julie D. King</h3>
-                  <span class="sub-title"></span>
-                  <p>“<em>Attended the Inks, Resin, Crystals, Stones, and Glitter class and loved it. Stephanie is knowledgeable, very nice and super accommodating. Lots of materials to choose from and lots of time to create and learn! I have already signed up for another class.</em>”</p>
-               </div>
-            </div>
-            </carousel>-->
-         </div>
-      </div>
-
-   </div>
-</div>
+    <div class="site-section block-13 testimonial-wrap">
+       <div class="row">
+          <div class="col-12 text-center">
+             <span class="sub-title">Happy Students</span>
+             <h2 class="font-weight-bold text-black mb-5">Testimonials</h2>
+          </div>
+       </div>
+       <div class="nonloop-block-13 owl-carousel owl-loaded owl-drag">
+          <div class="owl-stage-outer">
+             <!--<carousel>
+                <div class="owl-item active" style="width: 380px;">
+                   <div class="testimony">
+                      <img src="/images/reannin_sirianni.jpg" alt="Image" class="img-fluid">
+                      <h3>Reannin Sirianni</h3>
+                      <span class="sub-title"></span>
+                      <p>“<em>I recently took an art class here. It was ridiculously amazing. I had so much fun! I felt like a little kid creating my artwork. Stephanie is such an awesome teacher she teaches you many practical techniques but also gives you plenty of time to play around make a mess and encourages you to have fun. Stephanie’s energy is fabulous. I highly recommend taking a class here to everyone and anyone. I loved it!! I will definitely be back!</em>”</p>
+                   </div>
+                </div>
+                <div class="owl-item active" style="width: 380px;">
+                   <div class="testimony">
+                      <img src="/images/nellie_thompson.jpg" alt="Image" class="img-fluid">
+                      <h3>Nellie Thompson</h3>
+                      <span class="sub-title"></span>
+                      <p>“<em>I met Stephanie at a market and loved her creations. Took the flowers and landscape class. Loved it and can’t wait to take more.</em>”</p>
+                   </div>
+                </div>
+                <div class="owl-item active" style="width: 380px;">
+                   <div class="testimony">
+                      <img src="/images/julie_d_king.jpg" alt="Image" class="img-fluid">
+                      <h3>Julie D. King</h3>
+                      <span class="sub-title"></span>
+                      <p>“<em>Attended the Inks, Resin, Crystals, Stones, and Glitter class and loved it. Stephanie is knowledgeable, very nice and super accommodating. Lots of materials to choose from and lots of time to create and learn! I have already signed up for another class.</em>”</p>
+                   </div>
+                </div>
+                </carousel>-->
+             </div>
+       </div>
+    </div>
 
 
     <div class="site-section">
@@ -521,6 +519,11 @@ export default {
 }
 
 /* Good stuff here, pros on it */
+.hero-wrapper {
+  height: 100vh;
+  overflow: hidden;
+}
+
 #bg-video {
   position: absolute;
   width: 100vw;
@@ -531,4 +534,9 @@ export default {
   object-fit: cover;
 }
 
+/* Adjust section z-index to accommodate for video index */
+.site-section {
+  position: relative;
+  z-index: 1;
+}
 </style>
