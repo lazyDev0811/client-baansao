ea6cd0c7f7178e84890f5e1661f427fa95b81e7e
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-04-25 06:46:34 -0700

    Juggle layout blocks.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -58,72 +58,153 @@
     </div>
 
     <div class="site-section">
-        <div class="container pt-4">
-          <div class="row mt-4 mb-4 pt-4">
-            <div class="col-lg-6 mb-5 mb-lg-0">
-              <div class="img-border">
-                <img src="/images/oceanbeach.jpg" alt="Ocean Beach Art" class="img-fluid">
-              </div>
-            </div>
-            <div class="col-lg-5 ml-auto">
-              <span class="sub-title">What Types of Classes Do We Offer?</span>
-              <h2 class="font-weight-bold text-black mb-5">Popular Art Workshops:</h2>
-              <div class="accordion" id="accordionExample">
-
-                <h2 class="mb-0 border rounded mb-2">
-                  <button class="btn " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    1. Ink, Resins, and Crystals Art
-                  </button>
-                </h2>
-
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                  <div class="card-body">
-                    <p>Escape into the world of <a href="https://www.facebook.com/events/1951925708450518/">glassy resin, crystals and semi precious stones</a> such as Jasper, Turquoise, hematite, quartz, and more. In this 3-hour art escape, we discover how to create a vivid abstract alcohol-ink painting on a birch panel – with glitter, semi precious stones and crystals adding to the effects. We will also learn how to use resin on paintings (requiring a 12-hour cure time, to be picked up a later time). No prior experience needed, and all materials (including your choice of many crystals will be provided), and a handout will be emailed to you so you can replicate the techniques we practiced.</p>
-                  </div>
-                </div>
-
-                <h2 class="mb-0 border rounded mb-2">
-                  <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    2. Edgy Abstracts Art
-                  </button>
-                </h2>
-
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                  <div class="card-body">
-                    <p>We will be “living on the edge” as we delve into a world of <a href="https://www.facebook.com/events/2272133276332998/">industrial, graffiti type painting</a>. This is an exciting class where you get to immerse yourself into creative places you haven’t explored before. We will use various fluid mediums to create some really cool paintings that will not only be rebellious but will be exciting and beautiful to look at.</p>
-                  </div>
-                </div>
-
-                <h2 class="mb-0 border rounded mb-2">
-                  <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    3. Birch Trees Art Escape
-                  </button>
-                </h2>
-
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                  <div class="card-body">
-                    <p>In this art escape, we discover how to <a href="https://www.facebook.com/events/654900431606115/">paint a birch treeline</a> with a background on yupo paper mounted to a 9x12 birch panel. Time permitting, we also do some additional intuitive fluid painting on tiles as a bonus. You will receive lots of instruction, but will choose your own colours for the sky and leaves and any changes you desire.</p>
-                  </div>
-                </div>
-
-                <h2 class="mb-0 border rounded mb-2">
-                  <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                    4. Ocean in Resin Art Escape
-                  </button>
-                </h2>
-
-                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-                  <div class="card-body">
-                    <p>In this three hour art escape, we will <a href="https://www.facebook.com/events/258024471751180/">paint a cross section of the ocean</a>. Your water will have dimension and colour, foam and movement. We will also paint an art tile to have some reference to alcohol inks.</p>
-                  </div>
-                </div>
-
-              </div>
-
-            </div>
+      <div class="container pt-4">
+        <div class="row mt-4 mb-4 pt-4">
+          <div class="col-lg-11 ml-auto">
+            <span class="sub-title">What Types of Classes Do We Offer?</span>
+            <h2 class="font-weight-bold text-black mb-5">Popular Art Workshops:</h2>
           </div>
         </div>
       </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img
+                src="/images/inks_resin_crystal_glitter.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+            <h3 class="mb-0">
+              <a href="#">Ink, Resin and Crystals Art Escape</a>
+            </h3>
+            <br />
+            <p class="text-muted">
+              Escape into the world of glassy resin, crystals and semi precious
+              stones.
+            </p>
+            <p>
+              <a
+                href="contact.html"
+                class="btn btn-primary btn-lg rounded-0"
+                margin-top="20px"
+                >Learn More</a
+              >
+            </p>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img
+                src="/images/oceanbeach.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+            <h3 class="mb-0"><a href="#">Ocean in Resin Painting</a></h3>
+            <br />
+            <p class="text-muted">
+              In this three hour art escape, we will paint a cross section of
+              the ocean.
+            </p>
+            <p>
+              <a href="contact.html" class="btn btn-primary btn-lg rounded-0"
+                >Learn More</a
+              >
+            </p>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img
+                src="/images/edgy_abstract.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+            <h3 class="mb-0"><a href="#">Edgy Abstracts Art Escape</a></h3>
+            <br />
+            <p class="text-muted">
+              We will be “living on the edge” as we delve into a world of
+              industrial, graffiti type painting.
+            </p>
+            <p>
+              <a href="contact.html" class="btn btn-primary btn-lg rounded-0"
+                >Learn More</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container pt-4">
+        <div class="row mt-4 mb-4 pt-4">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="img-border">
+              <img src="/images/oceanbeach.jpg" alt="Ocean Beach Art" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-5 ml-auto">
+            <!-- Leave this in here useful for modal display -->
+            <!--<span class="sub-title">What Types of Classes Do We Offer?</span>
+            <h2 class="font-weight-bold text-black mb-5">Popular Art Workshops:</h2>-->
+            <div class="accordion" id="accordionExample">
+
+              <h2 class="mb-0 border rounded mb-2">
+                <button class="btn " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  1. Ink, Resins, and Crystals Art
+                </button>
+              </h2>
+
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                  <p>Escape into the world of <a href="https://www.facebook.com/events/1951925708450518/">glassy resin, crystals and semi precious stones</a> such as Jasper, Turquoise, hematite, quartz, and more. In this 3-hour art escape, we discover how to create a vivid abstract alcohol-ink painting on a birch panel – with glitter, semi precious stones and crystals adding to the effects. We will also learn how to use resin on paintings (requiring a 12-hour cure time, to be picked up a later time). No prior experience needed, and all materials (including your choice of many crystals will be provided), and a handout will be emailed to you so you can replicate the techniques we practiced.</p>
+                </div>
+              </div>
+
+              <h2 class="mb-0 border rounded mb-2">
+                <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  2. Edgy Abstracts Art
+                </button>
+              </h2>
+
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                  <p>We will be “living on the edge” as we delve into a world of <a href="https://www.facebook.com/events/2272133276332998/">industrial, graffiti type painting</a>. This is an exciting class where you get to immerse yourself into creative places you haven’t explored before. We will use various fluid mediums to create some really cool paintings that will not only be rebellious but will be exciting and beautiful to look at.</p>
+                </div>
+              </div>
+
+              <h2 class="mb-0 border rounded mb-2">
+                <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  3. Birch Trees Art Escape
+                </button>
+              </h2>
+
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                  <p>In this art escape, we discover how to <a href="https://www.facebook.com/events/654900431606115/">paint a birch treeline</a> with a background on yupo paper mounted to a 9x12 birch panel. Time permitting, we also do some additional intuitive fluid painting on tiles as a bonus. You will receive lots of instruction, but will choose your own colours for the sky and leaves and any changes you desire.</p>
+                </div>
+              </div>
+
+              <h2 class="mb-0 border rounded mb-2">
+                <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                  4. Ocean in Resin Art Escape
+                </button>
+              </h2>
+
+              <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                <div class="card-body">
+                  <p>In this three hour art escape, we will <a href="https://www.facebook.com/events/258024471751180/">paint a cross section of the ocean</a>. Your water will have dimension and colour, foam and movement. We will also paint an art tile to have some reference to alcohol inks.</p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
