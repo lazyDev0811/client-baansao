b8b6595061122a5fbb1fe109b89b61bed6a1758d
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-06-12 18:28:41 -0700

    Tweaks to ContentBlockLayout - conditionally hide elements.

--- src/components/layouts/ContentBlockLayout.vue
+++ src/components/layouts/ContentBlockLayout.vue
@@ -1,19 +1,19 @@
 <template>
   <div v-bind:class="classNameString">
-    <a @click="linkClicked" class="d-block figure">
+    <a v-if="typeof this.image === 'string' && this.image.length > 0" @click="linkClicked" class="d-block figure">
       <img
         :src="this.image"
         alt="Image"
         class="img-fluid brush-01"
       />
     </a>
-    <p class="text-muted">{{ this.datePublished }}</p>
+    <p v-if="typeof this.datePublished === 'string' && this.datePublished.length > 0" class="text-muted">{{ this.datePublished }}</p>
     <h3 class="mb-0">
       <a href="#">{{ this.title }}</a>
     </h3>
     <br />
     <p v-if="typeof this.description === 'string' && this.description.length > 0" class="text-muted" v-html="compiledDescription"></p>
-    <p>
+    <p v-if="typeof this.link === 'string' && this.link.length > 0">
       <a @click="linkClicked" v-bind:href="this.link" v-bind:aria-label="this.linkText" class="btn btn-art-class btn-lg rounded-0"><i class="material-icons block-icon">brush</i> {{ this.linkText }}</a>
     </p>
   </div>
