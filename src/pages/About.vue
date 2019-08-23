ca52c27fe9e530bad64f278527a61398db3d9982
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-08-22 17:35:59 -0700

    Change About page.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,10 @@
 <template>
   <Layout>
-    <div class="hero-wrapper angle">
+    <div class="">
       <div
-        v-if="getHeroById('page-hero') !== null"
-        class="site-blocks-cover inner-page text-shadow"
-        :style="`background-image: url(${getHeroById('page-hero').image});`"
-        data-aos="fade"
-        data-stellar-background-ratio="0.5"
+        class="properties-hero site-blocks-cover angle"
       >
-        <div class="container">
+        <div class="container" ref="pageHero">
           <div class="row align-items-center justify-content-center">
             <div class="col-md-10 text-center">
               <div class="hero-logo"><span></span></div>
@@ -22,96 +18,89 @@
     </div>
 
     <div class="content-wrapper">
-      <div v-if="getSection(0) !== null" class="site-section about-section">
-        <div class="container">
-          <div class="row align-items-center justify-content-center mt-5 mb-5 pb-5">
-            <div class="col-lg-6 img-years mb-5 mb-lg-0">
-              <img
-                :src="getSection(0).image"
-                alt="Image"
-                class="img-fluid circle float-left"
-              />
-            </div>
-            <div class="col-lg-6 ml-auto text-center">
-              <span class="sub-title" v-html="getSection(0).summary"></span>
-              <h3 class="mb-4" v-html="getSection(0).title"></h3>
-              <p class="mb-5" v-html="getSection(0).description"></p>
-              <p>
-                <a
-                  :href="getSection(0).link"
-                  target="_blank"
-                  class="btn btn-art-class btn-lg rounded-0">
-                  <i class="material-icons block-icon">brush</i> {{ getSection(0).linkText }}
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div v-if="getSection(1) !== null" class="site-section about-section">
-        <div class="container">
-          <div class="row align-items-center justify-content-center mb-5 pb-5">
-            <div class="col-lg-6 ml-auto text-center">
-              <span class="sub-title" v-html="getSection(1).summary"></span>
-              <h3 class="mb-4" v-html="getSection(1).title"></h3>
-              <p class="mb-5" v-html="getSection(1).description"></p>
-            </div>
-            <div class="col-lg-6 img-years mb-5 mb-lg-0">
-              <img
-                :src="getSection(1).image"
-                alt="Image"
-                class="img-fluid circle float-right"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div v-if="getSection(2) !== null" class="site-section about-section">
-        <div class="container">
-          <div class="row align-items-center justify-content-center mt-5 mb-5 pb-5">
-            <div class="col-lg-6 img-years mb-5 mb-lg-0">
-              <img
-                :src="getSection(2).image"
-                alt="Image"
-                class="img-fluid circle float-left"
-              />
-            </div>
-            <div class="col-lg-6 ml-auto text-center">
-              <span class="sub-title" v-html="getSection(2).summary"></span>
-              <h3 class="mb-4" v-html="getSection(2).title"></h3>
-              <p class="mb-5" v-html="getSection(2).description"></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="content-wrapper">
-      <div class="site-section">
-        <div class="">
-          <div class="row">
-            <div class="col-12 text-center mt-5">
-              <span class="sub-title">Testimonials from</span>
-              <h2 class="font-weight-bold text-black mb-5">Our Friends</h2>
-            </div>
-          </div>
-          <div class="slider row">
-            <div
-              v-for="testimonial in getTestimonialsByIndex(0, 3)"
-              class="col-sm-4 col-lg-3"
-            >
-              <testimonial-block-layout
-                className="testimony"
-                :reviewedBy="testimonial.reviewedBy"
-                :image="testimonial.image"
-                :testimonial="testimonial.testimonial"
-              />
-              <div
-                class="homepage-hero-action action-link-block text-center space-bottom">
-                <!--<a class="action-link space-top g-pstyle3" href="/lucas-profile" aria-label="My Work Experience">My Work Experience</a>-->
+      <div class="content-wrapper">
+        <div v-if="getSection(0) !== null" class="site-section about-section">
+          <div class="container">
+            <div class="row align-items-center justify-content-center mt-5 mb-5 pb-5">
+              <div class="col-lg-6 img-years mb-5 mb-lg-0">
+                <img
+                  :src="getSection(0).image"
+                  alt="Image"
+                  class="img-fluid circle float-left"
+                />
               </div>
+              <div class="col-lg-6 ml-auto text-center">
+                <span class="sub-title" v-html="getSection(0).summary"></span>
+                <h3 class="mb-4" v-html="getSection(0).title"></h3>
+                <p class="mb-5" v-html="getSection(0).description"></p>
+                <p>
+                  <a
+                    :href="getSection(0).link"
+                    target="_blank"
+                    class="btn btn-art-class btn-lg rounded-0">
+                    <i class="material-icons block-icon">brush</i> {{ getSection(0).linkText }}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="getSection(1) !== null" class="site-section about-section">
+          <div class="container">
+            <div class="row align-items-center justify-content-center mb-5 pb-5">
+              <div class="col-lg-6 ml-auto text-center">
+                <span class="sub-title" v-html="getSection(1).summary"></span>
+                <h3 class="mb-4" v-html="getSection(1).title"></h3>
+                <p class="mb-5" v-html="getSection(1).description"></p>
+              </div>
+              <div class="col-lg-6 img-years mb-5 mb-lg-0">
+                <img
+                  :src="getSection(1).image"
+                  alt="Image"
+                  class="img-fluid circle float-right"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="site-section" v-if="propertiesContent.length > 0">
+          <div class="container px-4">
+            <div class="row">
+              <content-block-layout
+                v-for="property in propertiesContent.slice(0, propertiesContent.length / 2)"
+                v-if="typeof property.summary === 'string'"
+                :key="property.id"
+                className="col-lg-6 col-md-6 mb-6 project-entry"
+                :title="property.title"
+                :description="(property.summary) ? property.summary : ''"
+                :link="`/property/${property.id}`"
+                linkText="Learn More"
+                :cloudinaryImage="property.imageId"
+                cloudName="baansaowanee"
+                :cloudFolder="property.galleryFolder"
+                :imageAlt="property.caption"
+              />
+
+              <span class="flower-separator-white"></span>
+            </div>
+            <div class="row mt-6 mb-6">
+              <content-block-layout
+                v-for="property in propertiesContent.slice(propertiesContent.length / 2, propertiesContent.length)"
+                v-if="typeof property.summary === 'string'"
+                :key="property.id"
+                className="col-md-4 col-lg-6 project-entry"
+                :title="property.title"
+                :description="(property.summary) ? property.summary : ''"
+                :link="`/property/${property.id}`"
+                linkText="Learn More"
+                :cloudinaryImage="property.imageId"
+                cloudName="baansaowanee"
+                :cloudFolder="property.galleryFolder"
+                :imageAlt="property.caption"
+              />
             </div>
           </div>
         </div>
@@ -121,64 +110,113 @@
 </template>
 
 <script>
-  // TODO: Implement page page that provides data interface?
+  import marked from 'marked';
+
   import ContentBlockLayout from '~/components/layouts/ContentBlockLayout.vue';
   import TestimonialBlockLayout from '~/components/layouts/TestimonialBlockLayout.vue';
 
+  import ThumbnailGallery from '~/core/components/ThumbnailGallery.vue';
+
+  // TODO: Implement page page that provides data interface?
   import HeroMixin from '~/mixins/HeroMixin';
   import SectionMixin from '~/mixins/SectionMixin';
+  import ServiceMixin from '~/core/mixins/ServiceMixin';
   import TestimonialMixin from '~/core/mixins/TestimonialMixin';
 
   // Import static data
-  import AboutData from '~/data/Company.yml';
+  import CompanyData from '~/data/Company.yml';
+
+  import * as ImageUtils from '~/core/utils/ImageUtils';
 
   export default {
-    metaInfo: {
-      title: "About"
-    },
     components: {
       ContentBlockLayout,
       TestimonialBlockLayout,
-      //VueGallerySlideshow
+      ThumbnailGallery
     },
-    mixins: [HeroMixin, SectionMixin, TestimonialMixin],
+    metaInfo: {
+      title: "Classes"
+    },
+    mixins: [HeroMixin, SectionMixin, ServiceMixin, TestimonialMixin],
     data() {
       return {
-        // TODO: Require this data format for pages
-        pageData: AboutData,
-        pageTitle: AboutData.title,
-        pageSubTitle: AboutData.subtitle
+        pageTitle: CompanyData.title,
+        pageData: CompanyData
       }
     },
     computed: {
+      propertiesContent() {
+        const content = this.$page.properties.edges.map(edge => {
+          const data = Object.assign({ id: edge.node.id }, edge.node.fields);
+          data.summary = (typeof window !== 'undefined') ? marked(data.summary) : data.summary;
+          data.description = (typeof window !== 'undefined') ? marked(data.description) : data.description;
+          return data;
+        });
+
+        return content;
+      },
       pageSectionContent() {
         console.log('dumping about data');
-        console.log(AboutData);
-        return (AboutData.hasOwnProperty('sections')) ? AboutData.sections : [];
+        console.log(CompanyData);
+        return (CompanyData.hasOwnProperty('sections')) ? CompanyData.sections : [];
+      },
+      serviceContent() {
+        return CompanyData;
       }
     }
-  };
+  }
 </script>
 
-<style>
-  .site-half .img-bg-1 {
-    background-size: cover;
-    background-position: center center;
-    background-repeat: no-repeat;
-    position: absolute;
-    width: 50%;
-    height: 100%;
+<page-query>
+  query Property {
+    properties: allProperty {
+      edges {
+        node {
+          id
+          fields {
+            image
+            imageId
+            gallery {
+              id
+              src
+              caption
+              #subCaption
+              featured
+            }
+            galleryFolder
+            link
+            linkText
+            title
+            metaKeywords
+            metaDescription
+            summary
+            description
+          }
+        }
+      }
+    }
+  }
+</page-query>
 
+
+<style lang="scss">
+  .page-wrap {
+    margin-top: 78px;
   }
 
-  @media screen and (min-width: 1280px) {
-    left: 20%;
-  }
-
-  .circle {
-    border-radius: 50%;
-    object-fit: cover;
-    width: 350px;
-    height: 350px;
+  .properties-hero {
+    height: auto !important;
+    min-height: 400px;
+    color: #222;
+    h1 {
+      color: #222;
+    }
+    .sub-text {
+      color: #999 !important;
+    }
+    .row {
+      height: auto;
+      min-height: 400px;
+    }
   }
 </style>
