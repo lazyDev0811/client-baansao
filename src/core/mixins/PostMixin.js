657d6680dc3279f00afdb71391d077874ed62895
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-08-24 15:16:00 -0700

    Tweak PostMixin, turn moment date parsing back on, it wasn't the issue with rendering.

--- src/core/mixins/PostMixin.js
+++ src/core/mixins/PostMixin.js
@@ -6,13 +6,12 @@ export default {
   computed: {
     postsContent() {
       const content = this.$page.posts.edges.map(edge => {
-        //let date = moment(edge.node.date);
+        let date = moment(edge.node.date);
 
         const content = Object.assign({}, edge.node, {
           summary: `${StringUtils.shortenText(edge.node.summary, 130)}...`,
           content: `${StringUtils.shortenText(edge.node.content, 320)}...`,
-          //date: date.format('MMM Do YYYY')
-          date: edge.node.date
+          date: date.format('MMM Do YYYY')
         });
 
         return content;
@@ -22,13 +21,12 @@ export default {
     },
     featuredContent() {
       const content = this.$page.featuredPosts.belongsTo.edges.map(edge => {
-        //let date = moment(edge.node.date);
+        let date = moment(edge.node.date);
 
         const content = Object.assign({}, edge.node, {
           summary: `${StringUtils.shortenText(edge.node.summary, 130)}...`,
           content: `${StringUtils.shortenText(edge.node.content, 320)}...`,
-          //date: date.format('MMM Do YYYY')
-          date: edge.node.date
+          date: date.format('MMM Do YYYY')
         });
 
         return content;
@@ -38,13 +36,12 @@ export default {
     },
     eventsContent() {
       const content = this.$page.eventsPosts.belongsTo.edges.map(edge => {
-        //let date = moment(edge.node.date);
+        let date = moment(edge.node.date);
 
         const content = Object.assign({}, edge.node, {
           summary: `${StringUtils.shortenText(edge.node.summary, 130)}...`,
           content: `${StringUtils.shortenText(edge.node.content, 320)}...`,
-          //date: date.format('MMM Do YYYY')
-          date: edge.node.date
+          date: date.format('MMM Do YYYY')
         });
 
         return content;
@@ -54,13 +51,12 @@ export default {
     },
     attractionsContent() {
       const content = this.$page.attractionsPosts.belongsTo.edges.map(edge => {
-        //let date = moment(edge.node.date);
+        let date = moment(edge.node.date);
 
         const content = Object.assign({}, edge.node, {
           summary: `${StringUtils.shortenText(edge.node.summary, 130)}...`,
           content: `${StringUtils.shortenText(edge.node.content, 320)}...`,
-          //date: date.format('MMM Do YYYY')
-          date: edge.node.date
+          date: date.format('MMM Do YYYY')
         });
 
         return content;
@@ -70,13 +66,12 @@ export default {
     },
     dealsContent() {
       const content = this.$page.dealsPosts.belongsTo.edges.map(edge => {
-        //let date = moment(edge.node.date);
+        let date = moment(edge.node.date);
 
         const content = Object.assign({}, edge.node, {
-          summary: `${StringUtils.shortenText(edge.node.summary, 130)}...`,
+          summary: `${StringUtils.shortenText(edge.node.summary, 90)}...`,
           content: `${StringUtils.shortenText(edge.node.content, 320)}...`,
-          //date: date.format('MMM Do YYYY')
-          date: edge.node.date
+          date: date.format('MMM Do YYYY')
         });
 
         return content;
