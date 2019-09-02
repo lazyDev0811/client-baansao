0cc34a8736aba3e23b2c52916428d29769421696
lazyDev0811 GIT_AUTHOR_EMAIL=dev.sun0811@gmail.com <dev.sun0811@gmail.com>
2019-09-01 21:56:30 -0700

    Change testimonial length...

--- src/components/layouts/TestimonialBlockLayout.vue
+++ src/components/layouts/TestimonialBlockLayout.vue
@@ -18,7 +18,7 @@
     ],
     computed: {
       trimmedTestimonial() {
-        return `${StringUtils.shortenText(this.testimonial, 270)}...`;
+        return `${StringUtils.shortenText(this.testimonial, 180)}...`;
       }
     }
   }
